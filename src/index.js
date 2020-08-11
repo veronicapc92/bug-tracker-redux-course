ac6a7928910b28220a45b5f4edc7dd0ec3144a7d
Veronica <veronicapc1992@gmail.com>
2020-08-11 15:21:33 +0100

    built the store, the reducer and the actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import store from "./store";
+import { bugAdded, bugRemoved } from "./actions";
+
+store.dispatch(bugAdded({ description: "Bug 1" }));
+
+store.dispatch(bugResolved({ id: 1 }));
+
+store.dispatch(bugRemoved({ id: 1 }));
