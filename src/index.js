ffc24525487f9f8bc5cfe830665dc9c764d1f53c
Veronica <veronicapc1992@gmail.com>
2020-08-12 10:15:40 +0100

    refactored the code using redux toolkit and created page slice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import store from "./store";
-import { bugAdded, bugRemoved } from "./actions";
+import configureStore from "./store/configureStore";
+import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import { projectAdded } from "./store/projects";
+
+const store = configureStore();
+
+store.dispatch(projectAdded({ name: "Project 1" }));
 
 store.dispatch(bugAdded({ description: "Bug 1" }));
 
---
+++ src/store/bugs.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// REDUCER
+let lastId = 0;
+
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) =>
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      }),
+
+    bugRemoved: (bugs, action) =>
+      bugs.filter((bug) => bug.id !== action.payload.id),
+
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+  },
+});
+
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export default slice.reducer;
